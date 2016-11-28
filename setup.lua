8a0382e32048ff3c7df1a5aed56b822cd8d66dc0
GuitarMusashi616 <guitarmusashi@gmail.com>
2016-11-27 20:00:01 -0900

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -9,9 +9,9 @@ local f = fs.open("update","w")
 f.write([[
 local tArgs = {...}
 if not tArgs[1] then
-	git.run("https://raw.githubusercontent.com/GuitarMusashi616/SchematicTurtle/"..Branch.."/setup.lua",{"update"})
+	git.run("https://raw.githubusercontent.com/GuitarMusashi616/SchematicTurtle/]]..Branch..[[/setup.lua",{"update"})
 else
-	git.run("https://raw.githubusercontent.com/GuitarMusashi616/SchematicTurtle/"..Branch.."/setup.lua",{"tArgs[1]"})
+	git.run("https://raw.githubusercontent.com/GuitarMusashi616/SchematicTurtle/]]..Branch..[[/setup.lua",{"tArgs[1]"})
 end
 ]])
 f.close()
