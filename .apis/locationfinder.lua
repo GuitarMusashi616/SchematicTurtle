686a01a80929c5c32aec66b98ddfdb85df0af652
GuitarMusashi616 <guitarmusashi@gmail.com>
2016-11-28 22:14:24 -0900

    Update locationfinder.lua

--- .apis/locationfinder.lua
+++ .apis/locationfinder.lua
@@ -1,7 +1,7 @@
 function checkIfAir()
 	--finds the location to place the next block
     while true do
-	iteratePro(1,1,1,height-1,width-1,length-1)
+	iteratePro(1,1,1,height,width,length)
 		--makes the turtle build faster by having to travel less
         blockID2 = getBlockId(x,y,z)	-- temporary variable 
         blockData2 = getData(x,y,z) 	-- temporary variable
