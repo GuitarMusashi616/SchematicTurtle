6ab37c49c38b31ac75d61c09c2ffc008c28a3766
GuitarMusashi616 <guitarmusashi@gmail.com>
2016-12-04 09:04:46 -0900

    Update locationfinder.lua

--- .apis/locationfinder.lua
+++ .apis/locationfinder.lua
@@ -26,9 +26,10 @@ function createInstructions(startx,starty,startz,height,width,length,nTurtles,pl
 	local nTurtles = nTurtles or 1
 	local startx,starty,startz = startx,starty,startz or 0,0,0
 	local placeMode = placeMode or "horizontal"
+	local oTurtles = {}
+	
 	
 	for i = 1,nTurtles do
-		local oTurtles = {}
 		oTurtles[i] = {}
 		oTurtles[i].responsibleLength =  math.floor(x/nTurtles)
 		oTurtles[tonumber(nTurtles)].responsibleLength = math.floor(x/nTurtles) + (x - (math.floor(x/nTurtles)*nTurtles))
